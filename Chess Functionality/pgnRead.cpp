14b44d52cbf12b5d32ec7eab00b2cec5ce903708
Dinoracks333 <40367952+Dinoracks333@users.noreply.github.com>
2023-03-30 23:16:17 -0400

    Add files via upload

---
+++ Chess Functionality/pgnRead.cpp
@@ -0,0 +1,181 @@
+//
+//  pgnRead.cpp
+//  
+//
+//  Created by Alejandro Casillas on 3/30/23.
+//
+
+#include "chessFuncs.cpp"
+#include <ctype.h>
+#include <string.h>
+
+
+char* fullLower(char* word){
+    int n=strlen(word);
+    char* low=new char[n+1];
+    for(int i=0;i<n;i++)
+        low[i]=tolower(word[i]);
+    low[n]=0;
+    return low;
+}
+
+int playPGN(char* seq){ //make sure not to have newline character in seq
+    //works!!
+    bool turn=0; //just setup stuff
+    char** move;
+    char** board=setUpBoard();
+    bool** hasMoved=setUpHM();
+    int x=0;
+    char enPas=10; //default value of 10 cause can't be accessed
+    char pgn[10];
+    int m=0;
+    int i;
+    for(i=0;seq[m]!=' ' && seq[m];i++,m++) //really glad I made this little thing. So much better than strtok (and doesnt change the original string)
+        pgn[i]=seq[m];
+    pgn[i]=0;
+    m++;
+    while(seq[m-1]){
+        //std::cout<<pgn<<"\n";
+        if(x%3==0){
+            x++;
+            for(i=0;seq[m]!=' ' && seq[m];i++,m++)
+                pgn[i]=seq[m];
+            pgn[i]=0;
+            m++;
+            continue;
+        }
+        move=pgnConvert(pgn,board,hasMoved,turn,enPas);
+        if(move==(char**) -1){ //I made this the bad value cause I made null be just a skipped turn...
+            std::cout<<"Invalid board\n";
+            return -2;
+        }
+        if(!move){
+            x++;
+            for(i=0;seq[m]!=' ' && seq[m];i++,m++)
+                pgn[i]=seq[m];
+            pgn[i]=0;
+            m++;
+            turn^=1;
+            continue;
+        }
+        if(move[2][0]==8){ //change en passant
+            enPas=move[1][1];
+        }
+        else{
+            enPas=10;
+        }
+        //printf("%d %d %d %d %d %d\n",move[0][0],move[0][1],move[1][0],move[1][1],move[2][0],move[2][1]);
+        movePiece(board,hasMoved,move,turn);
+        destroyMove(move);
+        turn^=1;
+        x++;
+        for(i=0;seq[m]!=' ' && seq[m];i++,m++)
+            pgn[i]=seq[m];
+        pgn[i]=0;
+        m++;
+    }
+
+    //moves done, check for result
+    if(strcmp(pgn,"1/2-1/2")==0){
+        //std::cout<<"Draw\n";
+        destroyBoard(board);
+        destroyHM(hasMoved);
+        return 2;
+    }
+    else if(strcmp(pgn,"1-0")==0){
+        //std::cout<<"White wins\n";
+        destroyBoard(board);
+        destroyHM(hasMoved);
+        return 0;
+    }
+    else if(strcmp(pgn,"0-1")==0){
+        //std::cout<<"Black wins\n";
+        destroyBoard(board);
+        destroyHM(hasMoved);
+        return 1;
+    }
+    else if(strcmp(pgn,"*")==0){
+        //std::cout<<"No result\n";
+        destroyBoard(board);
+        destroyHM(hasMoved);
+        return -1;
+    }
+    destroyBoard(board);
+    destroyHM(hasMoved);
+    return -1;
+}
+
+void rep(char* word){
+    for(int i=0;word[i];i++){
+        if(word[i]=='.'){
+            if(word[i+1]=='\n')
+                word[i]=0;
+            else
+                word[i]=' ';
+        }
+    }
+}
+
+//TOTAL GAME COUNT: 807765
+int main(){
+    //I think I've ironed out most of the problems!!! pretty happy. The hasMoved weird memory access thing was really problematic, but it is done now. No debugger necessary lol... might have helped though? idk. once I just figure out about where it was, I pretty much had it done mostly
+    FILE* fin;
+    /*
+    char temp[8000];
+    int res;
+    fin=fopen("testin.txt","r");
+    while(!feof(fin)){
+        fscanf(fin,"%[^\n]",temp);
+        fgetc(fin);
+        if(temp[0]=='1'){
+            res=playPGN(temp);
+            std::cout<<res<<"\n";
+            fgetc(fin);
+        }
+    }
+     */
+    FILE* fnames=fopen("chess_names.txt","r"); //all is working
+    char temp[8000]; //fuck it. 8kb. Somehow 2kb is not enough goddamnit
+    char tname[50];
+    char* lowname;
+    int fct=0;
+    int games=0;
+    while(!feof(fnames)){
+        fgets(tname,50,fnames);
+        rep(tname);
+        int i;
+        for(i=0;tname[i] && tname[i]!='\n';i++);
+        tname[i]='.';
+        tname[i+1]='p';
+        tname[i+2]='g';
+        tname[i+3]='n';
+        tname[i+4]='\0';
+        lowname=fullLower(tname);
+        fin=fopen(lowname,"r");
+        if(!fin){
+            std::cout<<lowname<<" couldnt be opened\n";
+            continue;
+        }
+        std::cout<<lowname<<" "<<++fct<<"\n";
+        delete[] lowname;
+        //int games=0;
+        int res;
+        while(!feof(fin)){
+            //std::cout<<++games<<"\n";
+            fscanf(fin,"%[^\n]",temp);
+            fgetc(fin);
+            if(temp[0]=='1'){
+                games++;
+                res=playPGN(temp);
+                //std::cout<<res<<"\n";
+                if(res==-2){
+                    std::cout<<temp<<"\n";
+                }
+                fgetc(fin);
+            }
+        }
+        fclose(fin);
+        std::cout<<games<<"\n";
+    }
+    return 0;
+}
